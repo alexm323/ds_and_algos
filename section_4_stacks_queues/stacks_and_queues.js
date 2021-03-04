915b535113c703227ee8728fbea9cf5bc81e0e2f
alexm323 <alejandromartinezjr42@gmail.com>
2021-03-03 20:41:47 -0800

    notes on stacks and queues

---
+++ section_4_stacks_queues/stacks_and_queues.js
@@ -0,0 +1,52 @@
+// const queue = [];
+
+// queue.push('Elie')
+// queue.push('Blue')
+
+// queue.shift()
+
+class Queue {
+    constructor() {
+        this.data = [];
+
+    }
+    enqueue(val){
+        this.data.push(val);
+    }
+    dequeue(val){
+        return this.data.shift();
+    }
+}
+
+const waitLine = new Queue();
+
+waitLine.enqueue('Charlie')
+waitLine.enqueue('David')
+waitLine.enqueue('Yang')
+
+
+class PriorityQueue{
+    constructor() {
+        this.data = [];
+    }
+    add(priority,value){
+        this.data.push({priority,value})
+    }
+    poll(){
+        let maxIdx = 0;
+        let maxPriority = this.data[0].priority;
+        for(let i = 1; i < this.data.length; i++){
+            if(this.data[i].priority > maxPriority){
+                maxPriority = this.data[i].priority;
+                maxIdx = i;
+            }
+        }
+        return this.data.splice(maxIdx,1)[0].value
+    }
+}
+
+const pq = new PriorityQueue();
+pq.add(8,'broken leg')
+pq.add(6,'dizziness')
+pq.add(10,'heart attack')
+pq.add(2,'tooth ache')
