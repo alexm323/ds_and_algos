d3b22fe05665cd1443d9f1031a69e702e382f539
alexm323 <alejandromartinezjr42@gmail.com>
2021-04-24 14:03:03 -0700

    basic trees assignment

--- section_7_trees/assignment/dsa-trees/binary-tree.js
+++ section_7_trees/assignment/dsa-trees/binary-tree.js
@@ -17,58 +17,99 @@ class BinaryTree {
    * the length of the shortest path from the root to a leaf. */
 
   minDepth() {
-
+    // check if there is a root at all
+    if (!this.root) return 0;
+    // create a helper function to help us count to a leaf node 
+    function minDepthHelper(node){
+      // if both the left and right children are null then we know that we are already on a leaf node on the root 
+      if(node.left === null && node.right === null) return 1;
+      // need if statements for the right and left children nodes 
+      // if the left is null then we try to traverse along the right but we recurse and call this function back on the right node and we add one to our count of the depth of the node 
+      if(node.left === null) return minDepthHelper(node.right) + 1;
+      // then for the opposite case 
+      if(node.right === null) return minDepthHelper(node.left) + 1;
+      // we want the min depth so we can use Math.min static method to see if the left or right path as the least and add 1 since we also have the root node to take into account
+      return (
+        Math.min(minDepthHelper(node.left),minDepthHelper(node.right)) +1
+      );
+    }
+    return minDepthHelper(this.root)
   }
 
   /** maxDepth(): return the maximum depth of the tree -- that is,
    * the length of the longest path from the root to a leaf. */
 
   maxDepth() {
+    // no root then we return 0
+    if(!this.root) return 0;
+    // lets make a function we can call recursively to find the longest chain of depth 
+    function maxDepthHelper(node){
+      // in the case the root node is our only node 
+      if(node.left === null && node.right === null) return 1;
+      // in case we come to a node that has a null left side 
+      if(node.left === null) return maxDepthHelper(node.right) + 1;
+      // and the inverse as well
+      if(node.right === null) return maxDepthHelper(node.left) + 1;
 
+      return(
+        Math.max(maxDepthHelper(node.left),maxDepthHelper(node.right)) + 1
+      );
+    }
+    return maxDepthHelper(this.root);
   }
 
   /** maxSum(): return the maximum sum you can obtain by traveling along a path in the tree.
    * The path doesn't need to start at the root, but you can't visit a node more than once. */
 
   maxSum() {
-
+    // lets keep track of the result of the sum 
+    let sum = 0;
+    // create a helper we can call recursively 
+    function maxSumHelper(node){
+      // if the node is null then we no there is no sum to be found 
+      if(node === null) return 0;
+      // need to track the left and right sums 
+      const leftSum = maxSumHelper(node.left);
+      const rightSum = maxSumHelper(node.right);
+      // we want to give the result the maximum value of all of our values 
+      sum = Math.max(sum, node.val + leftSum + rightSum);
+      // want to make sure we are taking the optimal path so we return the max value 
+      return Math.max(0,leftSum + node.val, rightSum + node.val)
+    }
+    maxSumHelper(this.root);
+    return sum;
   }
 
   /** nextLarger(lowerBound): return the smallest value in the tree
    * which is larger than lowerBound. Return null if no such value exists. */
 
-  nextLarger(lowerBound) {
+   nextLarger(lowerBound) {
+    //  if we are on the root then there can be no next smaller value
+    if (!this.root) return null;
+    // create a queue to use Breadth first search 
+    let queue = [this.root];
+    // set the closes value variable
+    let closest = null;
+    // while there are items in the queue we want to check the nodes
+    while (queue.length) {
+      // remove and return the first element of the queue with shift
+      let currentNode = queue.shift();
+      // set the current value
+      let currentVal = currentNode.val;
+      // find whether or not the value is higher than the lower bound
+      let higherThanLowerBound = currentVal > lowerBound;
+      // assign the closest smaller value 
+      let shouldReassignClosest = currentVal < closest || closest === null;
+      
+      if (higherThanLowerBound && shouldReassignClosest) {
+        closest = currentVal;
+      }
 
-  }
+      if (currentNode.left) queue.push(currentNode.left);
+      if (currentNode.right) queue.push(currentNode.right);
+    }
 
-  /** Further study!
-   * areCousins(node1, node2): determine whether two nodes are cousins
-   * (i.e. are at the same level but have different parents. ) */
-
-  areCousins(node1, node2) {
-
-  }
-
-  /** Further study!
-   * serialize(tree): serialize the BinaryTree object tree into a string. */
-
-  static serialize() {
-
-  }
-
-  /** Further study!
-   * deserialize(stringTree): deserialize stringTree into a BinaryTree object. */
-
-  static deserialize() {
-
-  }
-
-  /** Further study!
-   * lowestCommonAncestor(node1, node2): find the lowest common ancestor
-   * of two nodes in a binary tree. */
-
-  lowestCommonAncestor(node1, node2) {
-    
+    return closest;
   }
 }
 
--- section_7_trees/assignment/dsa-trees/tree.js
+++ section_7_trees/assignment/dsa-trees/tree.js
@@ -5,6 +5,7 @@ class TreeNode {
     this.val = val;
     this.children = children;
   }
+  
 }
 
 class Tree {
@@ -14,21 +15,74 @@ class Tree {
 
   /** sumValues(): add up all of the values in the tree. */
 
-  sumValues() {
+  sumValues(){
+    if(!this.root) return 0;
     
+    let total = this.root.val;
+
+    function sumHelper(node){
+      // want to go through each child of a node 
+      for(let child of node.children){
+        // grab the value and add it to the total
+        total += child.val;
+        // check for any children
+        if(child.children.length > 0){
+          // then we reapply the sum helper using that child as a node 
+          sumHelper(child);
+        }
+      }
+    }
+    sumHelper(this.root);
+    return total;
   }
+
 
   /** countEvens(): count all of the nodes in the tree with even values. */
 
   countEvens() {
-
+    // if there is no root then we just return 0
+    if(!this.root) return 0;
+    // if the initial root is the only node and it is even then we set the count to 1 , otherwise we start at 0
+    let evenCount = this.root.val % 2 === 0 ? 1: 0;
+    // create a helper function and accept a node as an argument
+    function countEvensHelper(node){
+      // loop through the children of the node we pass in 
+      for(let child of node.children){
+        // check the value of the child and if it is even add to the even count
+        if(child.val % 2 ===0) evenCount ++;
+        // check if that child has children and then recurse through the child node with the even counter if it has children
+        if(child.children.length > 0){
+          countEvensHelper(child)
+        }
+      }
+    }
+    // call the helper function on our root node 
+    countEvensHelper(this.root);
+    // return the count of the even numbers in the tree 
+    return evenCount;
   }
 
   /** numGreater(lowerBound): return a count of the number of nodes
    * whose value is greater than lowerBound. */
 
   numGreater(lowerBound) {
-
+    // if no root then we return 0
+    if(!this.root) return 0;
+    // set a variable for greater count, if there is onl;y one node (the root) and it is greater then the lower bound then we only have 1 val greater than the argument , otherwise we start at 0
+    let greaterCount = this.root.val > lowerBound ? 1: 0;
+    // create a help function that we can use to count child nodes 
+    function greaterCountHelper(node){
+      // loop through the children
+      for(let child of node.children){
+        // if the value is greater than the lower bound lets add one to the count
+        if(child.val > lowerBound) greaterCount++;
+        if(child.children.length > 0){
+          greaterCountHelper(child)
+        }
+      }
+    }
+    greaterCountHelper(this.root);
+    return greaterCount;
   }
 }
 
