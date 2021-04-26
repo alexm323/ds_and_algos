5eac6327d6d3109253336ae496f28d7696aa848c
alexm323 <alejandromartinezjr42@gmail.com>
2021-04-26 13:43:22 -0700

    notes on graphs and uses complete

---
+++ section_9_graphs/notes.js
@@ -0,0 +1,103 @@
+class PersonNode{
+    constructor(name, adjacent = new Set()){
+        this.name = name;
+        this.adjacent = adjacent;
+    }
+
+}
+class FriendGraph{
+    constructor(){
+        this.nodes = new Set();
+    }
+    addPerson(node){
+        this.nodes.add(node);
+    }
+    addPeople(peopleList){
+        for(let node of peopleList){
+            console.log(node)
+            this.addPerson(node);
+        }
+    }
+    setFriends(person1,person2){
+        person1.adjacent.add(person2);
+        person2.adjacent.add(person1);
+    }
+    areConnectedBFS(person1,person2){
+        let toVisitQueue = [person1];
+        let seen = new Set(toVisitQueue);
+        while(toVisitQueue.length){
+            // if the person we are on
+            let currentPerson = toVisitQueue.shift();
+            console.log(`Visiting`,currentPerson.name)
+            // if that is equal to the person we are looking for, that means there is a connection so we return true 
+            if(currentPerson === person2) return true;
+
+            for(let neighbor of currentPerson.adjacent){
+                if(!seen.has(neighbor)){
+                    toVisitQueue.push(neighbor);
+                    seen.add(neighbor);
+                }
+                
+            }
+        }
+        return false;
+    }
+    areConnectedDFS(person1,person2){
+        let toVisitStack = [person1];
+        let seen = new Set(toVisitStack);
+        while(toVisitStack.length){
+            // if the person we are on
+            let currentPerson = toVisitStack.pop();
+            console.log(`Visiting`,currentPerson.name)
+            // if that is equal to the person we are looking for, that means there is a connection so we return true 
+            if(currentPerson === person2) return true;
+
+            for(let neighbor of currentPerson.adjacent){
+                if(!seen.has(neighbor)){
+                    toVisitStack.push(neighbor);
+                    seen.add(neighbor);
+                }
+                
+            }
+        }
+        return false;
+    }
+    areConnectedRecursive(person1,person2, seen= new Set([person1])){
+        if(person1 === person2) return true;
+        // for each neighbor we are going to call this same function but we need to keep the seen persisting
+        for (let neighbor of person1.adjacent){
+            // if we have not seen this neighbor
+            if(!seen.has(neighbor)){
+                seen.add(neighbor);
+                if(this.areConnectedRecursive(neighbor,person2,seen)){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
+
+const homer = new PersonNode('homer simpson');
+const marge = new PersonNode('marge simpson');
+const maggie = new PersonNode('maggie simpson');
+const lisa = new PersonNode('lisa simpson');
+const grampa = new PersonNode('grampa simpson');
+
+const friends = new FriendGraph();
+friends.addPeople([homer,marge,maggie,lisa,grampa]);
+friends.setFriends(homer,marge);
+friends.setFriends(homer,lisa);
+friends.setFriends(homer,maggie);
+friends.setFriends(marge,maggie);
+friends.setFriends(maggie,lisa);
+friends.setFriends(lisa,grampa);
+
+const moe = new PersonNode('moe');
+const barney = new PersonNode('barney');
+const lenny = new PersonNode('lenny');
+
+friends.addPeople([moe,barney,lenny])
+friends.setFriends(moe,barney);
+friends.setFriends(barney,lenny);
+
