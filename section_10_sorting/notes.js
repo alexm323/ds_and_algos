04a0fb89e099a2eb020eaefb4f68839dd702830e
alexm323 <alejandromartinezjr42@gmail.com>
2021-04-30 10:44:33 -0700

    additional notes on merge sort and other sorting topics

--- section_10_sorting/notes.js
+++ section_10_sorting/notes.js
@@ -61,3 +61,47 @@ function bubbleSort3(arr){
 bubbleSort([45,6,7,23,1,19]);
 bubbleSort2([45,6,7,23,1,19]);
 bubbleSort3([45,6,7,23,1,19]);
+
+
+// lets do merge sort 
+
+// [5,9,20] [1,3,6]
+// we want to compare these values against each other and arrange them 
+
+function merge(arr1,arr2){
+    let results = [];
+    let i = 0;
+    let j = 0;
+    // going to compare the values at these arrays 
+
+    while(i < arr1.length && j < arr2.length){
+        if(arr1[i] < arr2[j]){
+            results.push(arr1[i]);
+            i++;
+        }else{
+            results.push(arr2[j]);
+            j++;
+        }
+    }
+    while(i < arr1.length){
+        results.push(arr1[i]);
+        i++;
+    }
+    while(j < arr2.length){
+        results.push(arr2[j]);
+        j++;
+    }
+    return results;
+
+}
+
+function mergeSort(arr){
+    //base case 
+    if(arr.length <= 1) return arr;
+    const mid = Math.floor(arr.length/2);
+    const left = mergeSort(arr.slice(0,mid));
+    const right  =  mergeSort(arr.slice(mid));
+    return merge(left,right);
+    //typical case
+
+}
